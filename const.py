6b840d3d1600da0033a905330dbc9539d84fc2a7
COMMON-PC\Common <shypulin@ukr.net>
2023-03-26 18:27:08 +0300

    Окончательно реорганизовано Main menu и работа с ним Оптимизирован код в плане удаления повторений

---
+++ const.py
@@ -0,0 +1,30 @@
+correct_input = '    Enter your choice: '
+
+incorrect_input = '    You choice is incorrect. Please try again: '
+
+menu_param = {0: {'header': 'Main menu',
+                  'header_indent': 3,
+                  'num_rjust': 0,
+                  'column_padding': 0,
+                  'row_count': 7},
+              1: {'header': 'List of movie genres',
+                  'header_indent': 3,
+                  'num_rjust': 2,
+                  'column_padding': 0,
+                  'row_count': 5},
+              2: {'header': 'List of music genres',
+                  'header_indent': 3,
+                  'num_rjust': 3,
+                  'column_padding': 0,
+                  'row_count': 15},
+              3: {'header': 'List of game genres',
+                  'header_indent': 0,
+                  'num_rjust': 0,
+                  'column_padding': 0,
+                  'row_count': 7}
+              }
+
+submenu_1_list = ['Action', 'Adventure', 'Animation', 'Biography', 'Comedy', 'Crime', 'Documentary',
+                  'Drama', 'Family', 'Fantasy', 'Film Noir', 'History', 'Horror', 'Music', 'Musical',
+                  'Mystery', 'Romance', 'Sci - Fi', 'Short Film', 'Sport', 'Superhero', 'Thriller',
+                  'War', 'Western']
