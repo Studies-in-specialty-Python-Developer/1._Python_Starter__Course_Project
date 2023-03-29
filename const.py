109d53c5d2377ca3cc8c7398c5833c10b8533c97
COMMON-PC\Common <shypulin@ukr.net>
2023-03-29 23:04:09 +0300

    Backup перед реорганизацией меню

--- const.py
+++ const.py
@@ -3,26 +3,22 @@ correct_input = '    Enter your choice: '
 incorrect_input = '    You choice is incorrect. Please try again: '
 
 menu_param = {0: {'header': 'Main menu',
-                  'header_indent': 3,
-                  'num_rjust': 0,
-                  'column_padding': 0,
-                  'row_count': 7},
+                  'header_indent': 3, 'num_rjust': 0, 'column_padding': 0, 'row_count': 7},
               1: {'header': 'List of movie genres',
-                  'header_indent': 3,
-                  'num_rjust': 2,
-                  'column_padding': 0,
-                  'row_count': 5},
+                  'header_indent': 4, 'num_rjust': 2, 'column_padding': 0, 'row_count': 5},
               2: {'header': 'List of music genres',
-                  'header_indent': 3,
-                  'num_rjust': 3,
-                  'column_padding': 0,
-                  'row_count': 15},
-              3: {'header': 'List of game genres',
-                  'header_indent': 0,
-                  'num_rjust': 0,
-                  'column_padding': 0,
-                  'row_count': 7}
+                  'header_indent': 5, 'num_rjust': 3, 'column_padding': 0, 'row_count': 15},
+              3: {'header': 'List of joke category',
+                  'header_indent': 3, 'num_rjust': 0, 'column_padding': 0, 'row_count': 4},
+              4: {'header': 'Play the game',
+                  'header_indent': 3, 'num_rjust': 0, 'column_padding': 0, 'row_count': 5},
               }
+
+pyjokes_genre = {
+    1: 'neutral',
+    2: 'chuck',
+    3: 'all',
+}
 
 submenu_1_list = ['Action', 'Adventure', 'Animation', 'Biography', 'Comedy', 'Crime', 'Documentary',
                   'Drama', 'Family', 'Fantasy', 'Film Noir', 'History', 'Horror', 'Music', 'Musical',
--- main.py
+++ main.py
@@ -1,209 +1,150 @@
+import json
 import math
 import os
 import sys
-import requests
-from bs4 import BeautifulSoup
+from pprint import pprint
 from prettytable import PrettyTable
 import colorama
 from colorama import Fore, Style  # , Back
 import spotipy
+from pyjokes import pyjokes
 from spotipy.oauth2 import SpotifyClientCredentials
-
 import const
+import conf_const
 
 
 def main():
-    print_menu('Main menu', menu_txt['Main menu'], 3, 0, 7)
-    menu_choice(main_menu_action)
-    pass
+    main_menu(0)
 
 
-def print_menu(header, items, header_indent, items_indent, rows):
-    pt = PrettyTable(border=False, header=False, left_padding_width=items_indent)
-    row_number = rows
-    col_number = math.ceil(len(items) / row_number)
-    while len(items) < row_number * col_number:
+def print_menu(menu_numb):
+    header: str = ''
+    items: list = []
+    row_count = const.menu_param.get(menu_numb)['row_count']
+    for header, items in menu_txt.get(menu_numb).items():
+        pass
+    column_count = math.ceil(len(items) / row_count)
+    while len(items) < row_count * column_count:
         items.append('')
-    for col in range(col_number):
-        pt.add_column(f'Column {col}', items[col * row_number:(col + 1) * row_number], align='l')
+    pt = PrettyTable(border=False, header=False, left_padding_width=const.menu_param.get(menu_numb)['column_padding'])
+    for col in range(column_count):
+        pt.add_column(f'Column {col}', items[col * row_count:(col + 1) * row_count], align='l')
     os.system('cls')
-    indent = ''.ljust(header_indent)
-    print(Fore.BLUE + Style.DIM + f'{indent}{header}')
+    print(Fore.BLUE + Style.DIM + f'{" " * const.menu_param[menu_numb]["header_indent"]}{header}')
     print(pt)
 
 
-def menu_choice(menu_action):
+def menu_choice(menu_numb):
     correct_choice = False
     input_str = const.correct_input
     while not correct_choice:
-        choice = input(input_str)
-        if choice.isdigit() and int(choice) < len(menu_action):
-            menu_action[choice](choice)
-            correct_choice = True
-
-
-def main_menu():
-    pass
-    # main_menu_txt = [f'{number}. {item}' for number, item in enumerate(const.main_menu_list, 1)]
-    # correct_choice = False
-    # input_str = const.correct_input
-    # while not correct_choice:
-    #     os.system('cls')
-    #     print(Fore.BLUE + Style.DIM + '  Main menu')
-    #     for item in main_menu_txt:
-    #         print(item)
-    #     print(Fore.RED + Style.DIM + '0. Exit')
-    #     print()
-    #     choice = input(input_str)
-    #     if choice.isdigit() and int(choice) < 3:
-    #         if choice == '0':  # 0. Exit
-    #             sys.exit(0)
-    #         if choice == '1':  # 1. Recommend the movie by genres
-    #             submenu1()
-    #         if choice == '2':  # 2. Recommend music by genres
-    #             submenu2()
-    #         if choice == '3':  # 3. Recommend the game by genres
-    #             submenu3()
-    #         # if choice == '4': # 4. Tell a joke
-    #         #     submenu4()
-    #         # if choice == '5': # 5. Tell an interesting story
-    #         #     submenu5()
-    #         # if choice == '6': # 6. Play the game
-    #         #     submenu6()
-    #         correct_choice = True
-    #     else:
-    #         input_str = const.incorrect_input
-    #         correct_choice = False
-
-
-def submenu1():
-    submenu_1_txt = [f'{number:>2}. {item}' for number, item in
-                     enumerate(const.submenu_1_list, 1)]
-    submenu_1_txt.append(' 0. Return')
-    submenu_1 = PrettyTable(border=False, header=False, left_padding_width=2)
-    row_number = 5
-    col_number = math.ceil(len(submenu_1_txt) / row_number)
-    while len(submenu_1_txt) < row_number * col_number:
-        submenu_1_txt.append('')
-    for col in range(math.ceil(len(submenu_1_txt) / row_number)):
-        submenu_1.add_column(f'Column {col}', submenu_1_txt[col * row_number:(col + 1) * row_number], align='l')
-    os.system('cls')
-    print(Fore.BLUE + Style.DIM + '  List of movie genres')
-    print(submenu_1)
-    print()
-    correct_choice = False
-    input_str = const.correct_input
-    while not correct_choice:
-        choice = input(input_str)
-        if choice.isdigit() and int(choice) < len(submenu_1_txt):
-            correct_choice = True
-            if choice == '0':
-                main_menu()
-            else:
-                submenu_1_proc(submenu_1_txt[int(choice) - 1])
+        if input_str == const.correct_input:
+            print(input_str, end=' ')
         else:
-            input_str = const.incorrect_input
-            correct_choice = False
-
-
-def submenu2():
-    pass
-    # submenu_2 = PrettyTable(border=False, header=False)
-    # row_number = 15
-    # col_number = math.ceil(len(submenu_2_txt) / row_number)
-    # while len(submenu_2_txt) < row_number * col_number:
-    #     submenu_2_txt.append('')
-    # for col in range(math.ceil(len(submenu_2_txt) / row_number)):
-    #     submenu_2.add_column(f'Column {col}', submenu_2_txt[col * row_number:(col + 1) * row_number], align='l')
-    # os.system('cls')
-    # print(Fore.BLUE + Style.DIM + '  List of music genres')
-    # print(submenu_2)
-    # print()
-    # correct_choice = False
-    # input_str = const.correct_input
-    # while not correct_choice:
-    #     choice = input(input_str)
-    #     if choice.isdigit() and int(choice) < len(submenu_2_txt):
-    #         correct_choice = True
-    #         if choice == '0':
-    #             main_menu()
-    #         else:
-    #             submenu_2_proc(submenu_2_txt[int(choice) - 1])
-    #     else:
-    #         input_str = const.incorrect_input
-    #         correct_choice = False
-
-
-def submenu3():
-    response = requests.get('https://ag.ru/genres')
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'lxml')
-    else:
-        print('Ошибка при получении данных о жанрах игр: код', response.status_code)
-
-    results = spotify.recommendation_genre_seeds()
-    submenu_2_txt = [f'{number:>3}. {item}' for number, item in
-                     enumerate(results.get('genres'), 1)]
-    submenu_2_txt.append('  0. Return')
-    submenu_2 = PrettyTable(border=False, header=False)
-    row_number = 15
-    col_number = math.ceil(len(submenu_2_txt) / row_number)
-    while len(submenu_2_txt) < row_number * col_number:
-        submenu_2_txt.append('')
-    for col in range(math.ceil(len(submenu_2_txt) / row_number)):
-        submenu_2.add_column(f'Column {col}', submenu_2_txt[col * row_number:(col + 1) * row_number], align='l')
-    os.system('cls')
-    print(Fore.BLUE + Style.DIM + '  List of music genres')
-    print(submenu_2)
-    print()
-    correct_choice = False
-    input_str = const.correct_input
-    while not correct_choice:
-        choice = input(input_str)
-        if choice.isdigit() and int(choice) < len(submenu_2_txt):
-            correct_choice = True
-            if choice == '0':
-                main_menu()
-            else:
-                submenu_2_proc(submenu_2_txt[int(choice) - 1])
+            print(Fore.RED + Style.DIM + input_str, end=' ')
+        choice = input()
+        if choice.isdigit() and int(choice) < len(menu_action.get(menu_numb)):
+            menu_action.get(menu_numb)[int(choice)](int(choice))
+            input_str = const.correct_input
         else:
-            input_str = const.incorrect_input
             correct_choice = False
+            input_str = const.incorrect_input
 
 
-def submenu_1_proc(genre):
-    pass
+def main_menu(menu_numb):
+    print_menu(menu_numb)
+    menu_choice(menu_numb)
 
 
-def submenu_2_proc(genre):
-    pass
+def submenu(menu_numb):
+    print_menu(menu_numb)
+    menu_choice(menu_numb)
+
+
+def menu_add(menu_numb: int, items: list) -> dict:
+    # Нумеруем пункты меню
+    numbering_items = [str(number).rjust(const.menu_param[menu_numb]["num_rjust"]) + '. ' + item
+                       for number, item in enumerate(items)]
+    # Делаем циклическую перестановку пунктов меню вправо на один шаг и добавляем заголовок
+    return {const.menu_param[menu_numb]["header"]: numbering_items[1:] + numbering_items[:1]}
+
+
+def de_numerate(menu_item: str) -> str:
+    return menu_item[menu_item.find('. ') + 2:]
+
+
+def get_movie(genre: int):
+    print(genre)
+
+
+def get_music(genre: int):
+    genre_txt = de_numerate(list(menu_txt.get(2).values())[0][genre - 1])
+    results = spotify.search(q='genre:' + genre_txt, type='artist').get('artists').get('items')
+    # for item in results:
+    pprint(results)
+
+
+def get_joke(genre: int):
+    print(pyjokes.get_joke(category=const.pyjokes_genre.get(genre)))
+
+
+def get_play(genre: int):
+    print(genre)
 
 
 if __name__ == "__main__":
     # Инициализация модуля colorama
     colorama.init(autoreset=True)
-    # Создание пунктов консольного меню в виде словаря
-    main_menu_txt = ['Recommend the movie by genres', 'Recommend music by genres', 'Recommend the game by genres',
-                     'Tell a joke', 'Tell an interesting story', 'Play the game']
 
-    spotify = spotipy.Spotify(
-        client_credentials_manager=SpotifyClientCredentials(client_id=const.client_id,
-                                                            client_secret=const.client_secret))
-    music_genres_txt = spotify.recommendation_genre_seeds()
+    # Формирование текстов консольного меню в виде списка словарей,
+    # у каждого меню свой номер, список пунктов и параметры
+    # Формирование списка действий консольного меню в виде списка словарей, у каждого списка свой номер
 
-    menu_txt = {'Main menu': [f'{number}. {item}' for number, item in enumerate(main_menu_txt, 1)]}
-    menu_txt['Main menu'].append('0. Exit')
-    menu_txt['Music genres'] = [f'{number:>3}. {item}' for number, item in enumerate(music_genres_txt.get('genres'), 1)]
-    menu_txt['Music genres'].append('  0. Return')
+    menu_txt = {}
+    menu_action = {}
 
-    main_menu_action = {
-        '0': sys.exit,  # 0. Exit colorama.deinit ()
-        '1': print,  # 1. Recommend the movie by genres
-        '2': print,  # 2. Recommend music by genres
-        '3': print,  # 3. Recommend the game by genres
-        '4': print,  # 4. Tell a joke
-        '5': print,  # 5. Tell an interesting story
-        '6': print  # 6. Play the game
+    menu_number = 0  # Main menu
+
+    menu_items = ['Exit', 'Recommend the movie by genres', 'Recommend music by genres',
+                  'Tell a joke', 'Play the game']
+    menu_txt[menu_number] = menu_add(menu_number, menu_items)
+    menu_action[menu_number] = {
+        0: sys.exit,  # 0. Exit
+        1: submenu,  # 1. Recommend the movie by genres
+        2: submenu,  # 2. Recommend music by genres
+        3: submenu,  # 3. Tell a joke
+        4: submenu,  # 4. Play the game
     }
 
+    menu_number = 1  # 1. Recommend the movie by genres
+
+    menu_items = ['Return']
+    menu_txt[menu_number] = menu_add(menu_number, menu_items)
+    menu_action[menu_number] = {i: main_menu if i == 0 else get_movie for i in range(1)}  # 0. Main menu 1-1. get_movie
+
+    menu_number = 2  # 2. Recommend music by genres
+
+    spotify = spotipy.Spotify(
+        client_credentials_manager=SpotifyClientCredentials(client_id=conf_const.spotify['client_id'],
+                                                            client_secret=conf_const.spotify['client_secret']))
+    menu_items = spotify.recommendation_genre_seeds().get('genres')
+    menu_items.insert(0, 'Return')
+    menu_txt[menu_number] = menu_add(menu_number, menu_items)
+    menu_action[menu_number] = {i: main_menu if i == 0 else get_music for i in range(len(menu_items))}
+    # 0. Main menu 1-125. get_music
+
+    menu_number = 3  # 3. Tell a joke
+
+    menu_items = ['Return', 'Neutral geek jokes', 'Chuck Norris geek jokes', 'All jokes']
+    menu_txt[menu_number] = menu_add(menu_number, menu_items)
+    menu_action[menu_number] = {i: main_menu if i == 0 else get_joke for i in range(len(menu_items))}
+    # 0. Main menu 1-3. get_joke
+
+    menu_number = 4  # 4. Play the game
+
+    menu_items = ['Return']
+    menu_txt[menu_number] = menu_add(menu_number, menu_items)
+    menu_action[menu_number] = {i: main_menu if i == 0 else get_play for i in range(1)}  # 0. Main menu 1-1. get_play
+    pprint(menu_txt)
+    pprint(menu_action)
     main()
